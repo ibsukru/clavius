eca6ee2f32b8da6927f711a47d3df73448c00db5
ibrahim velinov <ibsukru@gmail.com>
2020-12-02 20:44:37 +0100

    🚀

--- packages/clavius.web/pages/index.tsx
+++ packages/clavius.web/pages/index.tsx
@@ -1,19 +1,6 @@
 import React from 'react'
 import { Layout, Page } from '../components'
 import { storyBlokService } from '../lib'
-
-// const Index = () => {
-//   return (
-//     <Layout>
-//       <Navigation />
-//       <Headline />
-//       <Rails />
-//     </Layout>
-//   )
-// }
-
-// export default Index
-
 export default class extends React.Component<{}, { story: any }> {
   constructor(props) {
     super(props)
@@ -39,39 +26,9 @@ export default class extends React.Component<{}, { story: any }> {
   render() {
     const { content } = this.state.story
 
-    const { bg } = content
-
     return (
       <Layout>
-        <style jsx>{`
-          .hero {
-            position: relative;
-            width: 100vw;
-            height: 100vh;
-          }
-          .hero::before {
-            content: '';
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background: url(${bg.filename});
-            background-repeat: no-repeat;
-            background-size: cover;
-            background-position: center center;
-            filter: brightness(60%);
-          }
-          .hero-content {
-            position: relative;
-          }
-        `}</style>
-        <div className="herwo">
-          <div className="herwo-content">
-            <Page content={content} />
-          </div>
-        </div>
-        {/* <img className="hero" alt="hero" src={bg.filename} /> */}
+        <Page content={content} />
       </Layout>
     )
   }
