17c4c5df4c2defe8968d0ba7a38a0009fbc3a1cc
ibrahim velinov <ibsukru@gmail.com>
2020-12-08 16:27:56 +0100

    🚀

--- packages/clavius.lib/src/dealWithIt/dealWithIt.test.ts
+++
@@ -1,5 +0,0 @@
-import dealWithIt from './dealWithIt'
-
-test.each(['Hello world', undefined])('dealWithIt %s', text => {
-  expect(dealWithIt(text)).toMatchSnapshot()
-})
--- packages/clavius.lib/src/dealWithIt/dealWithIt.ts
+++
@@ -1,3 +0,0 @@
-const dealWithIt = (value?: string) => (value ? `${value} (▀̿Ĺ̯▀̿ ̿).` : '(▀̿Ĺ̯▀̿ ̿)')
-
-export default dealWithIt
