fc853abf6431016cdb6317f4a62154346c32bc57
ibrahim velinov <ibsukru@gmail.com>
2020-12-11 19:03:51 +0100

    🚀

--- packages/clavius.web/server/index.ts
+++ packages/clavius.web/server/index.ts
@@ -6,53 +6,53 @@ import bodyParser from 'body-parser'
 import next from 'next'
 import { storyBlokService } from 'clavius.lib/src'
 
-// // c(▀̿Ĺ̯▀̿ ̿).
-// ;(async (server: Application, app: Server) => {
-//   const port = process.env.PORT || '1123'
+const PORT = process.env.PORT || '1123'
 
-//   const handle = app.getRequestHandler()
-//   await app.prepare()
+// c(▀̿Ĺ̯▀̿ ̿).
+;(async (server: Application, app: Server) => {
+  const handle = app.getRequestHandler()
+  await app.prepare()
 
-//   server.use([
-//     bodyParser.json(),
-//     bodyParser.urlencoded({
-//       extended: true,
-//     }),
-//   ])
+  server.use([
+    bodyParser.json(),
+    bodyParser.urlencoded({
+      extended: true,
+    }),
+  ])
 
-//   server.get('/builder/:slug?', async (request, response, next) => {
-//     const sbService = storyBlokService()
-//     const slug = (request.params.slug as string) || 'home'
-//     if (/\.[^/.]+$/.test(slug)) return next()
+  server.get('/builder/:slug?', async (request, response, next) => {
+    const sbService = storyBlokService()
+    const slug = (request.params.slug as string) || 'home'
+    if (/\.[^/.]+$/.test(slug)) return next()
 
-//     try {
-//       request['storyblok'] = await sbService.get(
-//         `cdn/stories/${slug || 'home'}`,
-//         {},
-//       )
-//     } catch (error) {
-//       response.status(500)
+    try {
+      request['storyblok'] = await sbService.get(
+        `cdn/stories/${slug || 'home'}`,
+        {},
+      )
+    } catch (error) {
+      response.status(500)
 
-//       return app.render(request, response, '_error', {
-//         message: 'Something went wrong',
-//       })
-//     }
+      return app.render(request, response, '_error', {
+        message: 'Something went wrong',
+      })
+    }
 
-//     return app.render(request, response, '/builder')
-//   })
+    return app.render(request, response, '/builder')
+  })
 
-//   server.get('*', async (request, response) => {
-//     return handle(request, response)
-//   })
+  server.get('*', async (request, response) => {
+    return handle(request, response)
+  })
 
-//   server.listen(port, () => {
-//     console.info(`🚀> Ready on http://localhost:${port}`)
-//   })
-// })(
-//   express(),
-//   next({
-//     dev: process.env.NODE_ENV !== 'production',
-//     customServer: true,
-//     conf: baseConfig,
-//   }),
-// )
+  server.listen(PORT, () => {
+    console.info(`🚀> Ready on http://localhost:${PORT}`)
+  })
+})(
+  express(),
+  next({
+    dev: process.env.NODE_ENV !== 'production',
+    customServer: true,
+    conf: baseConfig,
+  }),
+)
