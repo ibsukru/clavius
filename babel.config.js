610199713d6a59f753a1348a592b40dd182c156d
ibrahim velinov <ibsukru@gmail.com>
2020-12-11 21:03:35 +0100

    🚀

--- babel.config.js
+++ babel.config.js
@@ -1,50 +1,50 @@
-const libs = ["clavius.lib", "clavius.web"];
+const libs = ['clavius.lib', 'clavius.web']
 
-const { NODE_ENV } = process.env;
+const { NODE_ENV } = process.env
 
 const alias = libs.reduce((acc, lib) => {
   return {
     ...acc,
     [`^${lib}/(.+)`]: ([, name]) => {
-      const src = name.replace(/^src/g, "dist");
+      const src = name.replace(/^src/g, 'dist')
 
-      return `${lib}/dist/${src.replace(/^dist/g, "")}`;
+      return `${lib}/dist/${src.replace(/^dist/g, '')}`
     },
-  };
-}, {});
+  }
+}, {})
 
 module.exports =
-  NODE_ENV === "test"
+  NODE_ENV === 'test'
     ? {
         presets: [
           [
-            "@babel/preset-react",
-            "@babel/preset-env",
+            '@babel/preset-react',
+            '@babel/preset-env',
             {
               targets: {
-                node: "current",
+                node: 'current',
               },
             },
           ],
-          "@babel/preset-typescript",
+          '@babel/preset-typescript',
         ],
       }
     : {
-        presets: ["next/babel", "@babel/preset-typescript"],
+        presets: ['next/babel', '@babel/preset-typescript'],
         plugins: [
           [
-            "module-resolver",
+            'module-resolver',
             {
-              root: ["."],
+              root: ['.'],
               alias,
             },
           ],
         ],
         ignore: [
           /node_modules/,
-          ...["test", "spec"]
-            .map((ext) => `**/*.${ext}.ts`)
+          ...['test', 'spec']
+            .map(ext => `**/*.${ext}.ts`)
             .reduce((acc, file) => acc.concat([file, `${file}x`]), [])
-            .concat(["**/__tests__", "**/__specs__"]),
+            .concat(['**/__tests__', '**/__specs__']),
         ],
-      };
+      }
