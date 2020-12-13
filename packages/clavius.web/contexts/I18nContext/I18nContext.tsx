a5c3f90df8576b9c72461a7ce52bd19a8a0d4db7
ibrahim velinov <ibsukru@gmail.com>
2020-12-13 23:07:57 +0100

    🚀

--- packages/clavius.web/contexts/I18nContext/I18nContext.tsx
+++ packages/clavius.web/contexts/I18nContext/I18nContext.tsx
@@ -1,13 +1,14 @@
 import { createContext, useState } from 'react'
-import { I18nContextContextType } from '.'
+import { I18nContextContextType, I18nContextContextProviderType } from '.'
 
 export const I18nContextContext = createContext<I18nContextContextType>({
   locale: 'en',
 })
 
-export const I18nContextContextProvider: React.FunctionComponent<{
-  locale: string
-}> = ({ children, ...rest }) => {
+export const I18nContextContextProvider: I18nContextContextProviderType = ({
+  children,
+  ...rest
+}) => {
   const [locale] = useState<string>(rest.locale)
 
   const value: I18nContextContextType = {
