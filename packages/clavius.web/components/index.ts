657c1e39d443b6e44ac5225046b5fc5cd3da4abe
ibrahim velinov <ibsukru@gmail.com>
2020-12-13 23:16:00 +0100

    🚀

--- packages/clavius.web/components/index.ts
+++ packages/clavius.web/components/index.ts
@@ -9,43 +9,9 @@ import Footer from './footer'
 import { Spinner } from './common'
 import DynamicComponent from './dynamicComponent'
 import Placeholder from './placeholder'
-import SbEditable, { SbEditableContent } from 'storyblok-react'
 import Error from './error'
-import { storyBlokComponentType } from '../contexts/featureToggleContext'
 
-export const EditableComponent = SbEditable
-
-export type EditableContentTpe = SbEditableContent
-
-export type InjectStoryBlokPropType<T> = React.FunctionComponent<{
-  blok: EditableContentTpe & T
-}>
-
-export type InjectStoryBlok<C, T> = (
-  Component: C,
-) => React.FunctionComponent<{
-  blok: EditableContentTpe & T
-}>
-
-export type TableBlokType = {
-  tbody: Array<{ body: Array<EditableContentTpe & { value: string }> }>
-}
-
-export type ImageStoryBlokType = {
-  title: string
-  asset: AssetBlokType
-  component: string
-}
-
-export type TitleStoryBlokKindType = 'h1' | 'h2' | 'h3'
-
-export type TitleStoryBlokType = {
-  text: string
-  kind: TitleStoryBlokKindType
-  component: string
-}
-
-export type AssetBlokType = { filename: string }
+export * from './index.types'
 
 export {
   Rails,
